edb690fa24910e97ecb810fc9885befdb133a79a
TusharChoudharyy <104813327+TusharChoudharyy@users.noreply.github.com>
2022-06-30 01:16:48 +0530

    Create checking number (+ve,-ve,0).cpp

---
+++ checking number (+ve,-ve,0).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number" << endl;
+    cin>>n;
+    if(n>0){
+    cout<<n<<"Print 1";
+}
+else if(n<0){
+    cout<<n<<"Print -1";
+}
+else{
+    cout<<"Print 0";
+}
+}
