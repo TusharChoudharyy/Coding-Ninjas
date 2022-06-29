64cdad3d2244304d9d9fece17fe9cfc463a4f443
TusharChoudharyy <104813327+TusharChoudharyy@users.noreply.github.com>
2022-06-30 02:09:12 +0530

    Create print even number.cpp

---
+++ print even number.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int N;
+    cin>>N;
+    cout<<"numbers are= "<<endl;
+    for(int i=1; i<=N; i++){
+    	if(i%2){
+    		 cout<<i<<endl;
+    	}
+       
+    }
+}
