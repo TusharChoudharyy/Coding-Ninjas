c4bd7649b773ab9d07a4f8cc2f29b157b56b0f09
TusharChoudharyy <104813327+TusharChoudharyy@users.noreply.github.com>
2022-06-30 00:05:59 +0530

    Create finding average of three number.cpp

---
+++ finding average of three number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1;
+    int n2;
+    int n3;
+    int average;
+    cin>>n1;
+    cin>>n2;
+    cin>>n3;
+    avg=(n1+n2+n3)/3;
+    cout<<average;
+}
